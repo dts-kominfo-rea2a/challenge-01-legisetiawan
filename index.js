79a8f224876905d5e35fe7138b0707376c466f56
legisetiawan <legi.setiawan@gmail.com>
2022-05-27 17:38:35 +0700

    mengisi seconduser dan memperbaiki firstuser

--- index.js
+++ index.js
@@ -8,15 +8,51 @@ const firstUser = {
     'email':'monica@dingdong.com',
     'favoriteColor':['Yellow','Pink','White','Purple'],
     'isHavePet':'Yes',
-    'education':{
-        'name':'SD 01',
-        'city':'Jakarta',
-        'graduate':2016
-    },
-    favoriteRestaurant:['Bento','Sushi']
-
+    'education':[{
+                        'name':'SD 01',
+                        'city':'Jakarta',
+                        'graduate':2016
+                    },{
+                        'name':'SMP 02',
+                        'city':'Jakarta',
+                        'graduate':2019
+                    },{
+                        'name':'SMA 03',
+                        'city':'Tangerang'
+                    },
+                ],
+    
+    favoriteRestaurant:['Bento','Sushi','Pancake','Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']
+    
 };
-const secondUser = {};
+
+const secondUser = {
+    'name':'Wendy',
+    'gender':'Male',
+    'age':23,
+    'email':'wendy@dingdong.com',
+    'favoriteColor':['Blue', 'Black', 'Grey'],
+    'isHavePet':'No',
+    'education':[{
+                        'name':'SD 02',
+                        'city':'Jakarta',
+                        'graduate':2010
+                    },{
+                        'name':'SMP 03',
+                        'city':'Bogor',
+                        'graduate':2013
+                    },{
+                        'name':'SMA 01',
+                        'city':'Surabaya',
+                        'graduate':2016
+                    },{
+                        'name':'Universitas Maju',
+                        'city':'Tangerang'
+                    },
+                ],
+
+    favoriteRestaurant:['Bento','Sushi','Pancake','Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
