2580ab67dccb3e041d361224be881f7917a201fb
legisetiawan <legi.setiawan@gmail.com>
2022-05-28 12:28:19 +0700

    memperbaiki data duplikat firstuser dan seconduser

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
+// MONICA
+let colorMonica = new Set();
+colorMonica.add('Yellow');
+colorMonica.add('Pink');
+colorMonica.add('White');
+colorMonica.add('Purple');
+let restaurantMonica = new Set()
+restaurantMonica.add('Bento');
+restaurantMonica.add("Sushi");
+restaurantMonica.add('Pancake');
+restaurantMonica.add('Eggy');
+restaurantMonica.add('Tempura');
+restaurantMonica.add('Bento');
+restaurantMonica.add('Eggy');
+restaurantMonica.add('Padang');
+restaurantMonica.add( 'Tteok');
+restaurantMonica.add('Sushi');
+restaurantMonica.add('Sushi');
+
 const firstUser = {
     'name':'Monica',
     'gender':'Female',
     'age':17,
     'email':'monica@dingdong.com',
-    'favoriteColor':['Yellow','Pink','White','Purple'],
+    'favoriteColor':[...colorMonica],
     'isHavePet':'Yes',
     'education':[{
                         'name':'SD 01',
@@ -22,16 +41,32 @@ const firstUser = {
                     },
                 ],
     
-    favoriteRestaurant:['Bento','Sushi','Pancake','Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']
+    favoriteRestaurant:[...restaurantMonica]
     
 };
+
+// WENDY
+let colorWendy = new Set();
+colorWendy.add('Blue');
+colorWendy.add('Black');
+colorWendy.add('Grey');
+let restaurantWendy = new Set()
+restaurantWendy.add('Tempura');
+restaurantWendy.add('Bento');
+restaurantWendy.add('Sushi');
+restaurantWendy.add('Pancake');
+restaurantWendy.add('Padang');
+restaurantWendy.add( 'Katsu');
+restaurantWendy.add('Geprek');
+restaurantWendy.add( 'Pancake');
+restaurantWendy.add('Eggy');
 
 const secondUser = {
     'name':'Wendy',
     'gender':'Male',
     'age':23,
     'email':'wendy@dingdong.com',
-    'favoriteColor':['Blue', 'Black', 'Grey'],
+    'favoriteColor':[...colorWendy],
     'isHavePet':'No',
     'education':[{
                         'name':'SD 02',
@@ -51,7 +86,7 @@ const secondUser = {
                     },
                 ],
 
-    favoriteRestaurant:['Bento','Sushi','Pancake','Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']
+    favoriteRestaurant:[...restaurantWendy]
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
